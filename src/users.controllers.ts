21562001a48803a04fb36178a919444deab5f4a6
priyanka-dudhat-27 <pdudhat27@gmail.com>
2024-08-23 17:18:50 +0530

    Route parameters/query parameters/sttuscode/header/redirect done

--- src/users.controllers.ts
+++ src/users.controllers.ts
@@ -1,11 +1,35 @@
-import {Controller, Get, Req} from "@nestjs/common"
-import {Request} from "express";
+import {Controller, Get, Req,HttpCode,HttpStatus,Res,Header,Redirect} from "@nestjs/common"
+import {Request,Response} from "express";
 
 @Controller("/users")
 export class UserController{
     @Get("/profile")
-    getProfile(@Req() req:Request){
-        console.log(req)
-        return "Hello Priyanka"
+    @Header("Cache-Control","none")
+    @Header("X-name","Priyanka")
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @Redirect("/users/account",302)
+    getProfile(@Req() req:Request,@Res({passthrough:true}) res:Response ){
+       const rn = Math.random()*10*1
+       if(rn>5){
+        return{
+            url:"/users/account",
+            statusCode:302
+        }
+       }else{
+        return{
+            url:"/users/wallet",
+            statusCode:302
+        }
+       }
+    }
+
+    @Get("/account")
+    redirectRoute(){
+        return "Working Account"
+    }
+
+    @Get("/wallet")
+    redirectWallet(){
+        return "Working Wallet"
     }
 }
